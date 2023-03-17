37ef37d94ba5bd0f88423f4b3f6559b8e099790a
My Name <myemail@example.com>
2023-03-17 14:00:37 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Retry point
+ * Return: Always 0
+ */
+int main(void)
+{
+char c;
+c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
