02c8e5ae1b0f30732d64a401c010fbd90aa28d35
My Name <myemail@example.com>
2023-03-29 13:49:20 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - check the code
@@ -24,7 +23,7 @@ p = &n;
    * - you are not allowed to code anything else than this line of code
    */
 p[5] = 98;
-/* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
